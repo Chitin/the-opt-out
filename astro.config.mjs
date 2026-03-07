d9c2d7434cdc2f6781ad010c3c9192d5911971ba
kolam <kolam1234>
2026-03-07 17:07:13 +0530

    rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://theopt.xyz",
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,15 +2,19 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
-export async function GET(context) {
-	const posts = await getCollection("blog");
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+export function GET(context) {
+  return rss({
+    // `<title>` field in output xml
+    title: SITE_TITLE,
+    // `<description>` field in output xml
+    description: SITE_DESCRIPTION,
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#site
+    site: import.meta.env.SITE,
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: import.meta.glob('./blog/**/*.md'),
+    // (optional) inject custom xml
+    customData: `<language>en</language>`,
+  });
 }
