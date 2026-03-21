bcbd5fff41e7f8780d19c5750ad42c42c4047853
kolam <kolam1234>
2026-03-21 15:32:41 +0530

    RSS, please work

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,6 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export const prerender = true;
@@ -16,12 +17,19 @@ export async function GET(context) {
 		site: context.site,
 		// Array of `<item>`s in output xml
 		// See "Generating items" section for examples using content collections and glob imports
-		items: posts
-			.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
-			.map((post) => ({
-				...post.data,
-				link: `/blog/${post.id}/`,
-			})),
+		items: await Promise.all(
+			posts
+				.sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
+				.map(async (post) => {
+					return {
+						...post.data,
+						link: `/blog/${post.id}/`,
+						content: sanitizeHtml(post.rendered.html, {
+							allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+						}),
+					};
+				}),
+		),
 		// (optional) inject custom xml
 		customData: `<language>en-us</language>`,
 	});
